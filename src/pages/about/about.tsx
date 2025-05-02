791bf8b726a2bf66f8488e038493dd059be80b1e
a-rudak-EIS <arudak@ebsco.com>
2025-05-02 15:21:25 +0500

    feat: remove changes

--- src/pages/about/about.tsx
+++
@@ -1,6 +0,0 @@
-const About = () => {
-  console.log('About page loaded!!');
-  return <>About</>;
-};
-
-export default About;
--- src/router/router.tsx
+++
@@ -1,6 +0,0 @@
-const Router = () => {
-  console.log('sss');
-  return <>Router</>;
-};
-
-export default Router;
